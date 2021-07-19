a581ce1a71db11da351eddfdc53a188100d392b3
bindubritto <sabbir.ruet.cse@gmail.com>
2021-07-19 09:11:55 +0600

    Internal service broken

--- 04-Networking-Between-Containers/external/external.js
+++ 04-Networking-Between-Containers/external/external.js
@@ -1,20 +1,52 @@
 'use strict';
 
 const express = require('express');
-const axios = require('axios');
 
 // Constants
-const PORT = 8080;
+const PORT = 8081;
 const HOST = '0.0.0.0';
 
 // App
 const app = express();
 
+const INTERNAL_API = "http://localhost:8080/api/v1/internal";
+
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World from Internal Service');
-});
+
+app.get('/', (request, response) => {
+  response.send('External service is running successfully')
+})
+
+
+// Making request to internal service
+
+app.get('/api/v1/external', async (request, response) => {
+
+  console.info('Requesting to ', INTERNAL_API);
+
+  try{
+    const internalResponse = await axios.get(INTERNAL_API);
+    console.log('internal response ', internalResponse.body);
+    
+    // const finalResponse = {
+    //   msg: 'communication successful',
+    //   response: internalResponse.data
+    // }
+
+    // send the response to the outer world
+    response.send(internalResponse.data);
+
+  } catch(err){
+    // handle the error
+    const errResponse = {
+      msg: 'error', err
+    }
+    response.send(err)
+  }
+})
+
+
 
 app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+console.log(`External service running on http://${HOST}:${PORT}`);
--- 04-Networking-Between-Containers/internal/internal.js
+++ 04-Networking-Between-Containers/internal/internal.js
@@ -4,17 +4,18 @@
 const express = require('express');
 
 // Constants
-const PORT = 8080
+const PORT = 8080;
 const HOST = '0.0.0.0'
 
 // App
-const app = express()
+const app = express();
 
 app.get('/', (request, response) => {
   response.send('Internal service is running successfully')
 })
 
 app.get('/api/v1/internal', (request, response) => {
+  console.log('request come form external service', request);
   response.send('Getting data from internal api');
 })
 
