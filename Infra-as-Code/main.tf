456f8e5de819258415e9b0fcccf41292813c9d66
Sabbir Ahmed <sabbir.ruet.cse@gmail.com>
2024-07-20 12:11:03 +0600

    Modifying resources, added new tags in resources

--- Infra-as-Code/main.tf
+++ Infra-as-Code/main.tf
@@ -8,12 +8,19 @@ provider "aws" {
 
 resource "aws_vpc" "development-vpc" {
   cidr_block = "10.0.0.0/16"
+  tags = {
+    Name: "development"
+    vpc_env: "dev"
+  }
 }
 
 resource "aws_subnet" "dev-subnet-1" {
   vpc_id = aws_vpc.development-vpc.id
   cidr_block = "10.0.10.0/24"
   availability_zone = "us-east-1a"
+  tags = {
+    Name: "subnet-1-dev"
+  }
 }
 
 // second apply
@@ -26,4 +33,10 @@ resource "aws_subnet" "dev-subnet-2" {
   vpc_id = data.aws_vpc.existing_vpc.id
   cidr_block = "172.31.48.0/20"
   availability_zone = "us-east-1a"
+  tags = {
+    Name: "subnet-2-default"
+  }
 }
+
+
+// 3rd apply - giving name to resources
