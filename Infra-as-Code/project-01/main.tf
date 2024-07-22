2adba08016b057779812c27b20166173737e86ea
Sabbir Ahmed <sabbir.ruet.cse@gmail.com>
2024-07-22 13:00:08 +0600

    Done automation of keypair to ssh into ec2

--- Infra-as-Code/project-01/main.tf
+++ Infra-as-Code/project-01/main.tf
@@ -7,7 +7,10 @@ variable subnet_cidr_block {}
 variable avail_zone {}
 variable env_prefix {}
 variable my_ip {}
-variable "instance_type" {}
+variable instance_type {}
+variable my_public_key {}
+variable public_key_location {}
+
 
 resource "aws_vpc" "myapp-vpc" {
   cidr_block = var.vpc_cidr_block
@@ -148,6 +151,15 @@ output "aws_ami_id" {
 }
 
 
+resource "aws_key_pair" "ssh-key" {
+  key_name = "server-key"
+
+  # public_key = var.my_public_key // way-01
+  # public_key = "${file("~/.ssh/id_rsa.pub")}" // way-02
+  public_key = file(var.public_key_location) // way-03
+}
+
+
 resource "aws_instance" "myapp-server" {
   ami = data.aws_ami.latest-amazon-linux-image.id
   instance_type = var.instance_type
@@ -158,9 +170,13 @@ resource "aws_instance" "myapp-server" {
   availability_zone = var.avail_zone
 
   associate_public_ip_address = true
-  key_name = "server-key-pair"
+  key_name = aws_key_pair.ssh-key.key_name
 
   tags = {
     Name: "${var.env_prefix}-server"
   }
+}
+
+output "ec2_public_ip" {
+  value = aws_instance.myapp-server.public_ip
 }
