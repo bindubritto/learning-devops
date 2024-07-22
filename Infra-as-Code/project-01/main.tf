f2df1019c4a7e031832f9842ea716c2a0761cebb
Sabbir Ahmed <sabbir.ruet.cse@gmail.com>
2024-07-22 12:09:11 +0600

    Done using defautl security group

--- Infra-as-Code/project-01/main.tf
+++ Infra-as-Code/project-01/main.tf
@@ -62,8 +62,41 @@ resource "aws_default_route_table" "default-rtb" {
   }
 }
 
-resource "aws_security_group" "myapp-sg" {
-  name = "myapp-sg"
+# resource "aws_security_group" "myapp-sg" {
+#   name = "myapp-sg"
+#   vpc_id = aws_vpc.myapp-vpc.id
+
+#   ingress {
+#     from_port = 22
+#     to_port = 22
+#     protocol = "tcp"
+#     cidr_blocks = [var.my_ip]
+#   }
+
+#   ingress {
+#     from_port = 8080
+#     to_port = 8080
+#     protocol = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+
+#   egress {
+#     from_port = 0
+#     to_port = 0
+#     protocol = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#     prefix_list_ids = [] // allowing access to vpc endpoints
+#   }
+
+#   tags = {
+#     Name: "${var.env_prefix}-sg"
+#   }
+# }
+
+
+# Using Default Security Group
+
+resource "aws_default_security_group" "default-sg" {
   vpc_id = aws_vpc.myapp-vpc.id
 
   ingress {
@@ -89,6 +122,6 @@ resource "aws_security_group" "myapp-sg" {
   }
 
   tags = {
-    Name: "${var.env_prefix}-sg"
+    Name: "${var.env_prefix}-default-sg"
   }
 }
