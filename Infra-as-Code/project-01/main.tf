c28d4683893e6dadcd3b635be100783d9a5f7964
Sabbir Ahmed <sabbir.ruet.cse@gmail.com>
2024-08-24 07:54:37 +0600

    modularize subnet

--- Infra-as-Code/project-01/main.tf
+++ Infra-as-Code/project-01/main.tf
@@ -2,15 +2,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-variable vpc_cidr_block {}
-variable subnet_cidr_block {}
-variable avail_zone {}
-variable env_prefix {}
-variable my_ip {}
-variable instance_type {}
-variable my_public_key {}
-variable public_key_location {}
-
 
 resource "aws_vpc" "myapp-vpc" {
   cidr_block = var.vpc_cidr_block
@@ -19,21 +10,17 @@ resource "aws_vpc" "myapp-vpc" {
   }
 }
 
-resource "aws_subnet" "myapp-subnet-1" {
+module "myapp-subnet" {
+  source = "./modules/subnet"
+
+  subnet_cidr_block = var.subnet_cidr_block
   vpc_id = aws_vpc.myapp-vpc.id
-  cidr_block = var.subnet_cidr_block
-  availability_zone = var.avail_zone
-  tags = {
-    Name: "${var.env_prefix}-subnet-1"
-  }
+  avail_zone = var.avail_zone
+  env_prefix = var.env_prefix
+  default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
 }
 
-resource "aws_internet_gateway" "myapp-igw" {
-  vpc_id = aws_vpc.myapp-vpc.id
-  tags = {
-    Name: "${var.env_prefix}-rtb"
-  }
-}
+
 
 # resource "aws_route_table" "myapp-route-table" {
 #   vpc_id = aws_vpc.myapp-vpc.id
@@ -52,19 +39,8 @@ resource "aws_internet_gateway" "myapp-igw" {
 #   route_table_id = aws_route_table.myapp-route-table.id
 # }
 
-## Use of default/main route table for our VPC
 
-resource "aws_default_route_table" "default-rtb" {
-  default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.myapp-igw.id
-  }
-  tags = {
-    Name: "${var.env_prefix}-default-igw"
-  }
-}
 
 # resource "aws_security_group" "myapp-sg" {
 #   name = "myapp-sg"
@@ -145,10 +121,6 @@ data "aws_ami" "latest-amazon-linux-image" {
 }
 
 
-## To see query results
-output "aws_ami_id" {
-  value = data.aws_ami.latest-amazon-linux-image.id
-}
 
 
 resource "aws_key_pair" "ssh-key" {
@@ -165,7 +137,7 @@ resource "aws_instance" "myapp-server" {
   instance_type = var.instance_type
 
   ## Optional Argument
-  subnet_id = aws_subnet.myapp-subnet-1.id
+  subnet_id = module.myapp-subnet.subnet.id
   vpc_security_group_ids = [aws_default_security_group.default-sg.id]
   availability_zone = var.avail_zone
 
@@ -177,8 +149,4 @@ resource "aws_instance" "myapp-server" {
   tags = {
     Name: "${var.env_prefix}-server"
   }
-}
-
-output "ec2_public_ip" {
-  value = aws_instance.myapp-server.public_ip
 }
